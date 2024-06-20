e50258c8fbd9255731b844cad256fa89729e791d
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2024-06-20 09:24:15 -0300

    Criação do Projeto - branch main

---
+++ src/roupa/Menu.java
@@ -0,0 +1,17 @@
+package roupa;
+
+//import java.util.Scanner;
+
+public class Menu {
+	public static void main(String[] args) {
+
+	}
+    
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: ");
+		System.out.println("Wallacy Gabriel Menezes Santos");
+		System.out.println("github.com/PhysisOfWally/Comercio-Eletronico-Projeto");
+		System.out.println("*********************************************************");
+	}
+}
